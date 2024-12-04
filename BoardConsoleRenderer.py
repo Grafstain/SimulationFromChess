7264947cdece7b4e291650d14d8a3b1798e6c0cb
Grafstain <grafstain@gmail.com>
2024-12-04 15:20:13 +0300

    render update, add some print info for creatures

--- BoardConsoleRenderer.py
+++ BoardConsoleRenderer.py
@@ -1,3 +1,5 @@
+from Grass import Grass
+from Stone import Stone
 from Board import Board
 from Coordinates import Coordinates
 from Herbivore import Herbivore
@@ -8,8 +10,6 @@ class BoardConsoleRenderer:
     ANSI_RESET = "\u001B[0m"
     ANSI_WHITE_SQUARE_BACKGROUND = "\u001B[47m"
     ANSI_BLACK_SQUARE_BACKGROUND = "\u001B[0;100m"
-    ANSI_YELLOW_ENTITY_COLOR = "\u001B[33m"
-    ANSI_RED_ENTITY_COLOR = "\u001B[31m"
     ANSI_WHITE_ENTITY_COLOR = "\u001B[97m"
     ANSI_BLACK_ENTITY_COLOR = "\u001B[30m"
     TAB = '\t'
@@ -17,7 +17,7 @@ class BoardConsoleRenderer:
     def render(self, board):
         # print("Rendering board...")
         for rank in range(8, 0, -1):
-            line = ""
+            line = f"{rank:2}  "
             for file in range(1, 9):  # Предположим, что файлы от 1 до 8
                 coordinates = Coordinates(file, rank)
                 if board.is_square_empty(coordinates):
@@ -25,9 +25,11 @@ class BoardConsoleRenderer:
                 else:
                     entity = board.get_piece(coordinates)
                     line += self.get_entity_sprite(entity)
-
             line += self.ANSI_RESET
             print(line)
+        # Выводим заголовок для столбцов
+        header = "    " + " ".join(f"{i + 1:3}" for i in range(board.width))  # Заголовок столбцов с выравниванием
+        print(header)
 
     def render_without_entity(self, board):
         self.render(board)
@@ -39,12 +41,28 @@ class BoardConsoleRenderer:
 
     def select_ascii_sprite_for_entity(self, entity) -> str:
         if isinstance(entity, Herbivore):
-            return f"{self.ANSI_WHITE_ENTITY_COLOR}🐑"
+            return "🐇"
         if isinstance(entity, Predator):
-            return f"{self.ANSI_RED_ENTITY_COLOR}🐺"
+            return "🐅"
+        if isinstance(entity, Grass):
+            return "🌾"
+        if isinstance(entity, Stone):
+            return "🌑"
+
 
     def get_entity_sprite(self, entity) -> str:
         sprite = f" {self.select_ascii_sprite_for_entity(entity)} "
         background_color = self.ANSI_BLACK_SQUARE_BACKGROUND if Board.is_square_dark(
             entity.coordinates) else self.ANSI_WHITE_SQUARE_BACKGROUND
         return f"{background_color}{sprite}{self.ANSI_RESET}"
+
+    @staticmethod
+    def display_log(board):
+        print("Объекты на поле:")
+        for rank in range(8, 0, -1):
+            for file in range(1, 9):
+                coordinates = Coordinates(file, rank)
+                if not board.is_square_empty(coordinates):
+                    entity = board.get_piece(coordinates)
+                    if isinstance(entity, (Herbivore, Predator)):
+                        print(f"{entity}\t{entity.coordinates}\t{entity.hp} energy")
