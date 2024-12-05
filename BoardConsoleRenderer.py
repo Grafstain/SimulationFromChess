cd607730b1459e745eec900df0fd4ab1c0fb9d0a
Grafstain <grafstain@gmail.com>
2024-12-05 18:37:26 +0300

    немного поправил рендер поля

--- BoardConsoleRenderer.py
+++ BoardConsoleRenderer.py
@@ -10,15 +10,13 @@ class BoardConsoleRenderer:
     ANSI_RESET = "\u001B[0m"
     ANSI_WHITE_SQUARE_BACKGROUND = "\u001B[47m"
     ANSI_BLACK_SQUARE_BACKGROUND = "\u001B[0;100m"
-    ANSI_WHITE_ENTITY_COLOR = "\u001B[97m"
-    ANSI_BLACK_ENTITY_COLOR = "\u001B[30m"
     TAB = '\t'
 
     def render(self, board):
         # print("Rendering board...")
-        for rank in range(8, 0, -1):
+        for rank in range(board.height, 0, -1):
             line = f"{rank:2}  "
-            for file in range(1, 9):  # Предположим, что файлы от 1 до 8
+            for file in range(1, board.width+1):  # Предположим, что файлы от 1 до 8
                 coordinates = Coordinates(file, rank)
                 if board.is_square_empty(coordinates):
                     line += self.get_sprite_for_empty_square(coordinates)
@@ -31,8 +29,6 @@ class BoardConsoleRenderer:
         header = "    " + " ".join(f"{i + 1:3}" for i in range(board.width))  # Заголовок столбцов с выравниванием
         print(header)
         # self.display_log(board)
-        self.display_common_creature_info(board)
-        print()
 
     def render_without_entity(self, board):
         self.render(board)
@@ -40,7 +36,7 @@ class BoardConsoleRenderer:
     def get_sprite_for_empty_square(self, coordinates: Coordinates) -> str:
         background_color = self.ANSI_BLACK_SQUARE_BACKGROUND if Board.is_square_dark(
             coordinates) else self.ANSI_WHITE_SQUARE_BACKGROUND
-        return f"{background_color}  {self.TAB}{self.ANSI_RESET}"
+        return f"{background_color}  {self.TAB}"
 
     def select_ascii_sprite_for_entity(self, entity) -> str:
         if isinstance(entity, Herbivore):
@@ -57,7 +53,7 @@ class BoardConsoleRenderer:
         sprite = f" {self.select_ascii_sprite_for_entity(entity)} "
         background_color = self.ANSI_BLACK_SQUARE_BACKGROUND if Board.is_square_dark(
             entity.coordinates) else self.ANSI_WHITE_SQUARE_BACKGROUND
-        return f"{background_color}{sprite}{self.ANSI_RESET}"
+        return f"{background_color}{sprite}"
 
     @staticmethod
     def display_log(board):
