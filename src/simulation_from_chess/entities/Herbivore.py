88b227460893ded44fbccbda781fccdd06f9f444
Grafstain <grafstain@gmail.com>
2024-12-07 13:01:54 +0300

    fix: -поправил инициализацию существ с параметрами из config.py

--- src/simulation_from_chess/entities/Herbivore.py
+++ src/simulation_from_chess/entities/Herbivore.py
@@ -1,13 +1,26 @@
-from ..core.Coordinates import Coordinates
-from ..entities.Grass import Grass
+from ..core import Coordinates
 from ..entities.Creature import Creature
+from ..entities.Grass import Grass
+from ..config import CREATURE_CONFIG
 
 
 class Herbivore(Creature):
-    def __init__(self,  coordinates: Coordinates):
-        super().__init__(coordinates, speed=1, hp=3)  # Примерные значения скорости и HP
-        self.target_type = Grass  # Определяем тип цели
+    def __init__(self, coordinates: Coordinates):
+        config = CREATURE_CONFIG['herbivore']
+        super().__init__(
+            coordinates=coordinates,
+            speed=config['speed'],
+            hp=config['initial_hp']
+        )
+        self.target_type = Grass
+        self.food_value = config['food_value']
 
     def __repr__(self):
         return f"Herbivore"
+
+    def eat(self, food):
+        """Восстановление HP при поедании травы."""
+        self.hp = min(CREATURE_CONFIG['herbivore']['initial_hp'],
+                     self.hp + self.food_value)
+        print(f"{self} съел {food} и восстановил {self.food_value} HP. Текущее HP: {self.hp}")
 
--- src/simulation_from_chess/entities/Predator.py
+++ src/simulation_from_chess/entities/Predator.py
@@ -1,13 +1,31 @@
 from ..core import Coordinates
 from ..entities.Herbivore import Herbivore
 from ..entities.Creature import Creature
+from ..config import CREATURE_CONFIG
 
 
 class Predator(Creature):
     def __init__(self, coordinates: Coordinates):
-        super().__init__(coordinates, speed=2, hp=4)  # Примерные значения скорости и HP
-        self.target_type = Herbivore  # Определяем тип цели
+        config = CREATURE_CONFIG['predator']
+        super().__init__(
+            coordinates=coordinates,
+            speed=config['speed'],
+            hp=config['initial_hp']
+        )
+        self.target_type = Herbivore
+        self.attack_damage = config['attack_damage']
+        self.food_value = config['food_value']
 
     def __repr__(self):
         return f"Predator"
+
+    def attack(self, target):
+        """Атака цели."""
+        target.take_damage(self.attack_damage)
+        if target.hp <= 0:
+            self.hp = min(CREATURE_CONFIG['predator']['initial_hp'],
+                         self.hp + self.food_value)
+            print(f"{self} съел {target} и восстановил {self.food_value} HP. Текущее HP: {self.hp}")
+
+
 
