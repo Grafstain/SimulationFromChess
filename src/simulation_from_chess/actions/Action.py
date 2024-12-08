6b459bf262dce2f9e4a414d9539690c3c30dbeaa
Grafstain <grafstain@gmail.com>
2024-12-08 08:45:32 +0300

    feat: обновлена система логирования - выводится информация по каждому существу в таблице

--- src/simulation_from_chess/actions/Action.py
+++ src/simulation_from_chess/actions/Action.py
@@ -1,4 +1,4 @@
 class Action:
-    def execute(self, board):
-        """Метод для выполнения действия на карте."""
-        raise NotImplementedError("Subclasses should implement this method.")
+    def execute(self, board, logger):
+        """Выполнить действие."""
+        pass
