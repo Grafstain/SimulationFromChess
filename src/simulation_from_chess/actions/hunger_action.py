a99a7e7451a24206fb88c42b9bada15d6bc8f236
Grafstain <grafstain@gmail.com>
2024-12-09 16:21:23 +0300

    refactor: HungerAction -добавил анотации и док стринг -убрал лишнее логирование

--- src/simulation_from_chess/actions/hunger_action.py
+++ src/simulation_from_chess/actions/hunger_action.py
@@ -1,14 +1,21 @@
 from ..actions.action import Action
 from ..entities.creature import Creature
+from ..core.board import Board
+from ..utils.logger import Logger
 
 
 class HungerAction(Action):
-    def __init__(self, hunger_damage=1):
+    def __init__(self, hunger_damage: int):
         self.hunger_damage = hunger_damage
 
-    def execute(self, board, logger):
-        """Наносит урон от голода всем существам на поле."""
+    def execute(self, board: Board, logger: Logger) -> None:
+        """
+        Применяет урон от голода ко всем существам.
+        
+        Args:
+            board: Игровое поле
+            logger: Логгер (не используется для голода)
+        """
         for entity in board.entities.values():
             if isinstance(entity, Creature):
                 entity.take_damage(self.hunger_damage)
-                logger.log_action(entity, "Голод", f"-{self.hunger_damage} HP")
