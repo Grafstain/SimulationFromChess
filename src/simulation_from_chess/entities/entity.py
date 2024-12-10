b6ecb8f5656a5626a31274f47ee4ac3e737dc47d
Grafstain <grafstain@gmail.com>
2024-12-10 12:28:23 +0300

    refactor: -some work for entities for save state turn -add entity id for future feat uniq names creature

--- src/simulation_from_chess/entities/entity.py
+++ src/simulation_from_chess/entities/entity.py
@@ -1,7 +1,13 @@
 from abc import ABC
 from ..core.coordinates import Coordinates
+from ..core.game_state import EntityState
 
 
 class Entity(ABC):
     def __init__(self, coordinates: Coordinates):
         self.coordinates = coordinates
+        self.entity_id = None  # Будет установлен при размещении на доске
+
+    def get_state(self) -> EntityState:
+        """Получение текущего состояния сущности."""
+        return EntityState(coordinates=self.coordinates)
