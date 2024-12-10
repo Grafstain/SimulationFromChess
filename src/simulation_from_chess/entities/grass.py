28e108fc26eefe26a1eb5a2c6e17855e1972e74d
Grafstain <grafstain@gmail.com>
2024-12-10 12:26:50 +0300

    refactor: -add repr for grass

--- src/simulation_from_chess/entities/grass.py
+++ src/simulation_from_chess/entities/grass.py
@@ -5,3 +5,7 @@ from ..entities.entity import Entity
 class Grass(Entity):
     def __init__(self, coordinates: Coordinates):
         super().__init__(coordinates)
+
+    def __repr__(self):
+        """Строковое представление травы."""
+        return "Трава"
