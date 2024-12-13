9d6768cd5697899bd54c275ccf248daa05510d14
Grafstain <grafstain@gmail.com>
2024-12-13 16:00:12 +0300

    refactor: -add Stone __repr__

--- src/simulation_from_chess/entities/stone.py
+++ src/simulation_from_chess/entities/stone.py
@@ -5,3 +5,7 @@ from ..entities.entity import Entity
 class Stone(Entity):
     def __init__(self, coordinates: Coordinates):
         super().__init__(coordinates)
+
+    def __repr__(self):
+        """Строковое представление камня."""
+        return "Камень"
