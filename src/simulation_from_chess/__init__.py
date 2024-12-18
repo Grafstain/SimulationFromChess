3df88ca8e584a11061b69bc8c59622db08b6107b
Grafstain <grafstain@gmail.com>
2024-12-18 17:59:35 +0300

    redactor: update __init__

--- src/simulation_from_chess/__init__.py
+++ src/simulation_from_chess/__init__.py
@@ -1,5 +1,7 @@
+from .actions.action import Action
 from .core import Board, Coordinates, Simulation
 from .entities import Herbivore, Predator, Grass, Stone
+from .entities.creature import Creature
 from .renderers import BoardConsoleRenderer
 from .actions import SpawnGrassAction, MoveAction, HealthCheckAction, HungerAction, InitAction
 from .config import SIMULATION_CONFIG, CREATURE_CONFIG
@@ -9,13 +11,13 @@ __all__ = [
     'Board', 'Coordinates', 'Simulation',
     
     # Entities
-    'Herbivore', 'Predator', 'Grass', 'Stone',
+    'Herbivore', 'Predator', 'Grass', 'Stone', 'Creature',
     
     # Renderers
     'BoardConsoleRenderer',
     
     # Actions
-    'SpawnGrassAction', 'MoveAction', 'HealthCheckAction', 'HungerAction', 'InitAction',
+    'SpawnGrassAction', 'MoveAction', 'HealthCheckAction', 'HungerAction', 'InitAction','Action',
     
     # Config
     'SIMULATION_CONFIG', 'CREATURE_CONFIG'
