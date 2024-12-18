2d0a8fb1ad9711e63461e881c59894f6fca9c41a
Grafstain <grafstain@gmail.com>
2024-12-18 18:06:29 +0300

    refactor Coordinates -добавлены свойства для сравнения координат

--- src/simulation_from_chess/core/coordinates.py
+++ src/simulation_from_chess/core/coordinates.py
@@ -3,16 +3,19 @@ class Coordinates:
         self.x = x
         self.y = y
 
-    def __eq__(self, other):
-        """Сравнение двух объектов Coordinates."""
-        if isinstance(other, Coordinates):
-            return self.x == other.x and self.y == other.y
-        return False
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Coordinates):
+            return False
+        return self.x == other.x and self.y == other.y
 
-    def __hash__(self):
-        """Хеширование для использования в словарях и множествах."""
+    def __hash__(self) -> int:
         return hash((self.x, self.y))
 
-    def __repr__(self):
-        """Строковое представление координат для удобства отладки."""
-        return f"Coordinates({self.x}, {self.y})"
+    def __lt__(self, other) -> bool:
+        """Определение операции 'меньше' для сравнения координат."""
+        if not isinstance(other, Coordinates):
+            return NotImplemented
+        return (self.x, self.y) < (other.x, other.y)
+
+    def __str__(self) -> str:
+        return f"({self.x}, {self.y})"
